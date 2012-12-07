cccb4e62b96039003b79c095205803a87da21b52
roger.castaldo@gmail.com <roger.castaldo@gmail.com@a0ea9d5d-1995-fdd3-4386-ad0dac0ceb3b>
2012-12-07 21:17:04 +0000

    corrected bug with improper namespace issues

--- trunk/Library/Constants.cs
+++ trunk/Library/Constants.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
 
-namespace Org.Reddragonit.FreeSwitch.Sockets
+namespace Org.Reddragonit.FreeSwitchSockets
 {
     internal class Constants
     {
--- trunk/Library/DialableNumber.cs
+++ trunk/Library/DialableNumber.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Org.Reddragonit.FreeSwitch.Sockets
+namespace Org.Reddragonit.FreeSwitchSockets
 {
     public class DialableNumber : IComparable
     {
